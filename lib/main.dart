275014b5de23a6a0705e0949e3694a6d8842169f
AlinaStepanova <astepanova@yukon.cv.ua>
2022-04-03 11:09:08 +0200

    remove redundant call

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:quotes/utils/constants.dart';
 
-import 'api/api.dart';
 import 'screens/home_page.dart';
 
 Future<void> main() async {
-  await API().initDio();
   runApp(MyApp());
 }
 
